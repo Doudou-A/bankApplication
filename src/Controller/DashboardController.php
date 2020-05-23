3f3f4cc4f7419daea3b43c92d4edf0c2301023ab
Doudou <adeldoudou1996@gmail.com>
2020-05-23 10:06:20 +0200

    ok

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Transaction;
+use Carbon\Carbon;
 use App\Repository\AccountRepository;
 use App\Repository\TransactionRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,20 +15,24 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(AccountRepository $repoAccount, TransactionRepository $repoTransaction):Response
+    public function index(AccountRepository $repoAccount, TransactionRepository $repoTransaction): Response
     {
         $user = $this->getUser();
         $accounts = $repoAccount->findBy(array('user' => $user));
         //$accounts = $repoAccount->findByUser($user);
         $transactions = new Transaction();
 
-        foreach($accounts as $account){
-        $transactions = $repoTransaction->findBy(array('account' => $account), array('dateCreated' => 'desc'),5,0);
+        foreach ($accounts as $account) {
+            $transactions = $repoTransaction->findBy(array('account' => $account), array('dateCreated' => 'desc'), 5, 0);
         }
+
+        $date = Carbon::parseFromLocale('demain', 'fr');
+        $dateTime = "Now: $date";
 
         return $this->render('dashboard/index.html.twig', [
             'accounts' => $accounts,
-            'transactions' => $transactions
+            'transactions' => $transactions,
+            'dateTime' => $dateTime
         ]);
     }
 }
